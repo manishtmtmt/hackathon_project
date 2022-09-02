b4dfb98f855fc14634b268e615931a2cac7bda24
Manish Tiwari <manisht.mt.mt@gmail.com>
2022-09-03 00:53:20 +0530

    crud routes for patients

--- backend/routes/patient.js
+++ backend/routes/patient.js
@@ -4,10 +4,16 @@ const patientRouter = Router();
 
 const PatientModel = require("../models/Patient");
 
-patientRouter.post("/create", async (req, res) => {
-  const data = req.body;
-  console.log(data)
-  const newPatient = new PatientModel(data);
+patientRouter.post("/create/:doctorId", async (req, res) => {
+  const { patientname, email, description, completed } = req.body;
+  const { doctorId } = req.params;
+  const newPatient = new PatientModel({
+    patientname,
+    email,
+    description,
+    completed,
+    doctorId,
+  });
   await newPatient.save();
   return res.status(200).send("Patient successfully booked the appointment");
 });
@@ -15,7 +21,36 @@ patientRouter.post("/create", async (req, res) => {
 patientRouter.get("/", async (req, res) => {
   const { specilization } = req.headers;
   const patients = await PatientModel.find({ specilization });
-  return res.status(200).send(patients)
+  return res.status(200).send(patients);
+});
+
+patientRouter.patch("/edit/:patientId", async (req, res) => {
+  const { patientId } = req.params;
+  const { completed } = req.body;
+  const updatedPatient = await PatientModel.findOneAndUpdate(
+    { _id: patientId },
+    { $set: { completed } },
+    { new: true }
+  );
+  return res.status(200).send(updatedPatient);
+});
+
+patientRouter.delete("/delete/:patientId", async (req, res) => {
+  const { patientId } = req.params;
+  await PatientModel.findOneAndDelete({ _id: patientId });
+  return res.status(200).send("Deleted successfully");
+});
+
+patientRouter.get("/queue/:doctorId", async (req, res) => {
+  const {doctorId} = req.params;
+  PatientModel.countDocuments({doctorId, completed: false}).exec((err, count) => {
+    if (err) {
+        res.send(err);
+        return;
+    }
+
+    res.json({ count: count });
+});
 });
 
 module.exports = patientRouter;
