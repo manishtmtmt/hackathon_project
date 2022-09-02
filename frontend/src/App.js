7d7d549a051aa146a890089f54fc17c29252eb40
Sai-Narasimha <bukkapatnam.sainarasimha@gmail.com>
2022-09-02 11:29:45 +0530

    users page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
-
-import './App.css';
-import Home from './components/Home';
+import "./App.css";
+import Home from "./components/Home";
+import { UserPage } from "./components/UserPage";
 
 function App() {
   return (
     <div className="App">
-     <h1>started</h1>
-     <Home />
+      <h1>started</h1>
+      {/* <Home /> */}
+      <UserPage />
     </div>
   );
 }
