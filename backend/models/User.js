df3c2d9420e1c4fcb762ed8622e717a8ba72ef23
Manish Tiwari <manisht.mt.mt@gmail.com>
2022-09-02 17:27:23 +0530

    get single doctor route done

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,7 +4,7 @@ const userSchema = new Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  role: { type: String, required: true, default: "user" },
+  role: { type: String, required: true, default: "doctor" },
 });
 
 const UserModel = model("user", userSchema);
